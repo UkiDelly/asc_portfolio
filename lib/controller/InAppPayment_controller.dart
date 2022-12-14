11fb3c005fb92905f519b4e757713cab796cc821
padonan <98295182+padonan@users.noreply.github.com>
2022-12-14 16:38:30 +0900

    Update InAppPayment_controller.dart

--- lib/controller/InAppPayment_controller.dart
+++ lib/controller/InAppPayment_controller.dart
@@ -1,5 +1,5 @@
 class InAppPaymentController {
-  final String webApplicationId = '63827d64cf9f6d00201b4a79';
-  final String androidApplicationId = '63827d64cf9f6d00201b4a7a';
-  final String iosApplicationId = '63827d64cf9f6d00201b4a7b';
+  final String webApplicationId = '';
+  final String androidApplicationId = '';
+  final String iosApplicationId = '';
 }
