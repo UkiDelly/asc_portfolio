071639c3457ef61d7cd9c8e2ded06761b5d98bd0
Dae Hyeon <daehyeon1999@naver.com>
2022-12-26 17:34:03 +0900

    Add GoRouter into a provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,12 @@
-import 'package:asc_portfolio/constant/enum/user/user_enum.dart';
-import 'package:asc_portfolio/pages/admin/admin_main_screen.dart';
-import 'package:asc_portfolio/pages/cafe/select_cafe_screen.dart';
-import 'package:asc_portfolio/pages/home/seat_screen.dart';
-import 'package:asc_portfolio/pages/login/login_screen.dart';
-import 'package:asc_portfolio/pages/main_screen.dart';
-import 'package:asc_portfolio/pages/payment/payment_screen.dart';
-import 'package:asc_portfolio/pages/signup/sign_up_screen.dart';
-import 'package:asc_portfolio/provider/home_state/home_state_notifier.dart';
 import 'package:asc_portfolio/service/notification_service.dart';
 import 'package:asc_portfolio/style/app_color.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:flutter_secure_storage/flutter_secure_storage.dart';
-import 'package:go_router/go_router.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:logger/logger.dart';
 
-import 'provider/secure_storage_provider.dart';
+import 'provider/go_router_provider.dart';
 
 void main() async {
   await initializeDateFormatting('ko_KR');
@@ -42,68 +31,7 @@ class MyApp extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final router = GoRouter(
-      initialLocation: '/login',
-      redirect: (context, state) async {
-        final bool userLogin = ref.watch(homeStateProvider).loginCheck;
-        final FlutterSecureStorage storage = ref.read(secureStorageProvider);
-
-        final RoleType roleType =
-            RoleTypeExtension.getRoleType(await storage.read(key: 'roleType') ?? '');
-
-        if (userLogin) {
-          if (roleType == RoleType.admin) {
-            return '/admin';
-          } else if (roleType == RoleType.user) {
-            return '/';
-          }
-        } else {
-          return '/login';
-        }
-        return null;
-      },
-      routes: [
-        GoRoute(
-          path: '/',
-          builder: (context, state) => const MainScreen(),
-          routes: [
-            GoRoute(
-              path: 'seat',
-              builder: (context, state) => const SeatScreen(),
-            ),
-          ],
-        ),
-        GoRoute(
-          path: '/login',
-          builder: (context, state) => const LoginScreen(),
-          redirect: (context, state) {
-            final userLogin = ref.watch(homeStateProvider).loginCheck;
-
-            if (userLogin) {
-              return '/';
-            } else {
-              return '/login';
-            }
-          },
-        ),
-        GoRoute(
-          path: '/sign_up',
-          builder: (context, state) => SignupPage(),
-        ),
-        GoRoute(
-          path: '/admin',
-          builder: (context, state) => const AdminMainPage(),
-        ),
-        GoRoute(
-          path: '/cafe_page',
-          builder: (context, state) => SelectCafeScreen(),
-        ),
-        GoRoute(
-          path: '/payment',
-          builder: (context, state) => const PaymentPage(),
-        ),
-      ],
-    );
+    final router = ref.watch(goRouterProvider);
 
     return MaterialApp.router(
       routerConfig: router,
