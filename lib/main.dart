d9c590790eb4852a490af744f2bda49737e9321c
padonan <bbde1861@gmail.com>
2022-12-31 20:41:40 +0900

    12 31 Firebase 연동

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,23 @@
 import 'package:asc_portfolio/pages/admin/admin_main_page.dart';
 import 'package:asc_portfolio/pages/home_page.dart';
 import 'package:asc_portfolio/pages/login/login_page.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MyApp());
+  var fcmToken = await FirebaseMessaging.instance.getToken(
+    vapidKey: "BBOfhbB8XaVcQDzGng2qYz2-PeqRGB9rzym_lEzJGQp9JUhBRnMaiqBEntIJH8AjEl3pzIS8_ylW6gcZXByl8L8"
+  );
+  print("fcmToken="+fcmToken.toString());
 }
+
 
 class MyApp extends StatelessWidget {
 
